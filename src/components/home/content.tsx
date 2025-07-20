4f14d2e2d15d885f3ece479db9b26433b62aed59
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-07-20 19:18:27 +0700

    style(home): adjust responsive layout for tablet and mobile

--- src/components/home/content.tsx
+++ src/components/home/content.tsx
@@ -2,104 +2,108 @@ import Image from "next/image";
 
 export default function HomeContent() {
     return (
-        <div className="flex flex-col gap-20 justify-center items-center p-[124px]">
-            <div className="max-w-[1192px] flex flex-col items-center gap-4">
+        <div className="flex flex-col gap-8 md:gap-16 lg:gap-20 justify-center items-center py-12 md:py-20 px-6 md:px-8 lg:p-[124px]">
+            <div className="max-w-[1192px] w-full flex flex-col items-center gap-4">
                 <div className="flex flex-row gap-2">
                     <div className="h-[22px] w-[1px] bg-primary-500" />
                     <p className="text-[14px] font-medium leading-[22px] text-primary-500">FEATURES</p>
                 </div>
                 <div className="flex flex-col max-w-[580px] gap-4 items-center">
-                    <h2 className="text-[48px] font-normal text-center text-grayscale-900 leading-[60px]">Your Last Manual Unit Test Was Yesterday</h2>
-                    <p className="text-[20px] leading-[36px] text-center text-grayscale-500">SoftQA crafts unit tests at enterprise scale, freeing your creativity for development and enlightening your code&apos;s testability.</p>
+                    <h2 className="text-[32px] leading-10 md:text-[48px] font-normal text-center text-grayscale-900 md:leading-[60px]">Your Last Manual Unit Test Was Yesterday</h2>
+                    <p className="text-[18px] md:text-[20px] leading-7 md:leading-[36px] text-center text-grayscale-500">SoftQA crafts unit tests at enterprise scale, freeing your creativity for development and enlightening your code&apos;s testability.</p>
                 </div>
             </div>
-            <div className="max-w-[1192px] grid grid-cols-2 gap-8">
-                <div className="rounded-[16px] bg-additional-unbleached flex flex-col max-w-[580px] w-full">
-                    <div className="flex flex-row">
-                        <div className="flex flex-col justify-between p-8 h-[296px] max-w-[324px] w-full border-b border-grayscale-100">
+            <div className="max-w-[1192px] grid grids-cols-1 lg:grid-cols-2 gap-8">
+                <div className="rounded-[16px] bg-additional-unbleached flex flex-col lg:max-w-[580px] w-full">
+                    <div className="flex flex-col-reverse md:flex-row">
+                        <div className="flex flex-col gap-2 md:justify-between p-6 md:p-8 md:h-[296px] lg:max-w-[324px] w-full border-b border-grayscale-100">
                             <div className="flex flex-row gap-2">
                                 <div className="h-[22px] w-[1px] bg-primary-500" />
                                 <p className="text-[14px] font-medium leading-[22px] text-primary-500">AUTOPILOT</p>
                             </div>
-                            <h4 className="text-[32px] leading-[40px] text-grayscale-900 font-normal underline">Autopilot for Unit Testing</h4>
+                            <h4 className="text-[24px] md:text-[32px] leading-[30px] md:leading-10 text-grayscale-900 font-normal underline">Autopilot for Unit Testing</h4>
                         </div>
-                        <div className="bg-additional-palm max-w-[256px] w-full rounded-tr-[16px] flex flex-col justify-center items-center">
+                        <div className="bg-additional-palm h-[316px] md:h-auto md:max-w-[256px] w-full rounded-t-[16px] md:rounded-tl-[0px] md:rounded-tr-[16px] flex flex-col justify-center items-center">
                             <Image
                                 src="/home/autopilot.png"
                                 alt="autopilot"
                                 width={180}
                                 height={180}
+                                className="w-[229.92px] h-[229.92px] md:w-[180px] md:h-[180px]"
                             />
                         </div>
                     </div>
-                    <div className="p-8">
-                        <p className="text-[18px] leading-8 font-normal text-grayscale-600">SoftQA analyzes your code and AI-generates precise unit tests, shifting your focus to developing exciting features while removing manual labor from testing processes.</p>
+                    <div className="px-6 py-4 md:p-8">
+                        <p className="text-[16px] md:text-[18px] leading-7 md:leading-8 font-normal text-grayscale-600">SoftQA analyzes your code and AI-generates precise unit tests, shifting your focus to developing exciting features while removing manual labor from testing processes.</p>
                     </div>
                 </div>
-                <div className="rounded-[16px] bg-primary-50 flex flex-col max-w-[580px] w-full">
-                    <div className="flex flex-row">
-                        <div className="flex flex-col justify-between p-8 h-[296px] max-w-[324px] w-full border-b border-grayscale-100">
+                <div className="rounded-[16px] bg-primary-50 flex flex-col lg:max-w-[580px] w-full">
+                    <div className="flex flex-col-reverse md:flex-row">
+                        <div className="flex flex-col gap-2 md:justify-between p-6 md:p-8 md:h-[296px] lg:max-w-[324px] w-full border-b border-grayscale-100">
                             <div className="flex flex-row gap-2">
                                 <div className="h-[22px] w-[1px] bg-primary-500" />
                                 <p className="text-[14px] font-medium leading-[22px] text-primary-500">ALL IN ONE</p>
                             </div>
-                            <h4 className="text-[32px] leading-[40px] text-grayscale-900 font-normal">One tool for all browser</h4>
+                            <h4 className="text-[24px] md:text-[32px] leading-[30px] md:leading-10 text-grayscale-900 font-normal">One tool for all browser</h4>
                         </div>
-                        <div className="bg-additional-light max-w-[256px] w-full rounded-tr-[16px] flex flex-col justify-center items-center">
+                        <div className="bg-additional-light h-[316px] md:h-auto md:max-w-[256px] w-full rounded-t-[16px] md:rounded-tl-[0px] md:rounded-tr-[16px] flex flex-col justify-center items-center">
                             <Image
                                 src="/home/allinone.png"
                                 alt="autopilot"
                                 width={180}
                                 height={180}
+                                className="w-[229.92px] h-[229.92px] md:w-[180px] md:h-[180px]"
                             />
                         </div>
                     </div>
-                    <div className="p-8">
-                        <p className="text-[18px] leading-8 font-normal text-grayscale-600">Supports browser testing on Chrome, Edge, Safari, Firefox, IE, Android, iOS, as well as mobile app testing for iOS, Android, and Flutter.</p>
+                    <div className="px-6 py-4 md:p-8">
+                        <p className="text-[16px] md:text-[18px] leading-7 md:leading-8 font-normal text-grayscale-600">Supports browser testing on Chrome, Edge, Safari, Firefox, IE, Android, iOS, as well as mobile app testing for iOS, Android, and Flutter.</p>
                     </div>
                 </div>
-                <div className="rounded-[16px] bg-additional-distant flex flex-col max-w-[580px] w-full">
-                    <div className="flex flex-row">
-                        <div className="flex flex-col justify-between p-8 h-[296px] max-w-[324px] w-full border-b border-grayscale-100">
+                <div className="rounded-[16px] bg-additional-distant flex flex-col lg:max-w-[580px] w-full">
+                    <div className="flex flex-col-reverse md:flex-row">
+                        <div className="flex flex-col gap-2 md:justify-between p-6 md:p-8 md:h-[296px] lg:max-w-[324px] w-full border-b border-grayscale-100">
                             <div className="flex flex-row gap-2">
                                 <div className="h-[22px] w-[1px] bg-primary-500" />
                                 <p className="text-[14px] font-medium leading-[22px] text-primary-500">MAXIMIZED</p>
                             </div>
-                            <h4 className="text-[32px] leading-[40px] text-grayscale-900 font-normal">Test Coverage, Maximized</h4>
+                            <h4 className="text-[24px] md:text-[32px] leading-[30px] md:leading-10 text-grayscale-900 font-normal">Test Coverage, Maximized</h4>
                         </div>
-                        <div className="bg-additional-salty max-w-[256px] w-full rounded-tr-[16px] flex flex-col justify-center items-center">
+                        <div className="bg-additional-salty h-[316px] md:h-auto md:max-w-[256px] w-full rounded-t-[16px] md:rounded-tl-[0px] md:rounded-tr-[16px] flex flex-col justify-center items-center">
                             <Image
                                 src="/home/maximize.png"
                                 alt="autopilot"
                                 width={180}
                                 height={179}
+                                className="w-[229.92px] h-[228.64px] md:w-[180px] md:h-[179px]"
                             />
                         </div>
                     </div>
-                    <div className="p-8">
-                        <p className="text-[18px] leading-8 font-normal text-grayscale-600">SoftQA automates code analysis and generates precise unit tests, freeing you to focus on developing exciting features without manual testing.</p>
+                    <div className="px-6 py-4 md:p-8">
+                        <p className="text-[16px] md:text-[18px] leading-7 md:leading-8 font-normal text-grayscale-600">SoftQA automates code analysis and generates precise unit tests, freeing you to focus on developing exciting features without manual testing.</p>
                     </div>
                 </div>
-                <div className="rounded-[16px] bg-additional-whitest flex flex-col max-w-[580px] w-full">
-                    <div className="flex flex-row">
-                        <div className="flex flex-col justify-between p-8 h-[296px] max-w-[324px] w-full border-b border-grayscale-100">
+                <div className="rounded-[16px] bg-additional-whitest flex flex-col lg:max-w-[580px] w-full">
+                    <div className="flex flex-col-reverse md:flex-row">
+                        <div className="flex flex-col gap-2 md:justify-between p-6 md:p-8 md:h-[296px] lg:max-w-[324px] w-full border-b border-grayscale-100">
                             <div className="flex flex-row gap-2">
                                 <div className="h-[22px] w-[1px] bg-primary-500" />
                                 <p className="text-[14px] font-medium leading-[22px] text-primary-500">FAST</p>
                             </div>
-                            <h4 className="text-[32px] leading-[40px] text-grayscale-900 font-normal">Testing at Warp Speed</h4>
+                            <h4 className="text-[24px] md:text-[32px] leading-[30px] md:leading-10 text-grayscale-900 font-normal">Testing at Warp Speed</h4>
                         </div>
-                        <div className="bg-additional-snow max-w-[256px] w-full rounded-tr-[16px] flex flex-col justify-center items-center">
+                        <div className="bg-additional-snow h-[316px] md:h-auto md:max-w-[256px] w-full rounded-t-[16px] md:rounded-tl-[0px] md:rounded-tr-[16px] flex flex-col justify-center items-center">
                             <Image
                                 src="/home/fast.png"
                                 alt="autopilot"
                                 width={180}
                                 height={180}
+                                className="w-[229.92px] h-[229.92px] md:w-[180px] md:h-[180px]"
                             />
                         </div>
                     </div>
-                    <div className="p-8">
-                        <p className="text-[18px] leading-8 font-normal text-grayscale-600">SoftQA swiftly generates unit tests for your entire codebase by pointing it to a directory. Boost productivity and maximize coding minutes with our bulk test generation.</p>
+                    <div className="px-6 py-4 md:p-8">
+                        <p className="text-[16px] md:text-[18px] leading-7 md:leading-8 font-normal text-grayscale-600">SoftQA swiftly generates unit tests for your entire codebase by pointing it to a directory. Boost productivity and maximize coding minutes with our bulk test generation.</p>
                     </div>
                 </div>
             </div>
--- src/components/home/testimonials.tsx
+++ src/components/home/testimonials.tsx
@@ -2,37 +2,51 @@ import Image from "next/image";
 
 export default function HomeTestimonials() {
     return (
-        <div className="flex flex-col justify-center items-center px-[124px] pt-[124px] pb-[132px] border-y">
-            <div className="max-w-[1192px] flex flex-col gap-16 items-center justify-center">
-                <div className="max-w-[1192px] flex flex-col gap-4 items-center px-[306px]">
-                <div className="flex flex-row gap-2">
-                    <div className="h-[22px] w-[1px] bg-primary-500" />
-                    <p className="text-[14px] font-medium leading-[22px] uppercase text-primary-500">we love developers</p>
+        <div className="flex flex-col justify-center items-center px-6 md:px-8 lg:px-[124px] py-12 md:py-20 lg:pt-[124px] lg:pb-[132px] border-y">
+            <div className="max-w-[1192px] flex flex-col gap-10 md:gap-12 lg:gap-16 items-center justify-center">
+                <div className="max-w-[580px] flex flex-col gap-3 md:gap-4 items-center">
+                    <div className="flex flex-row gap-2">
+                        <div className="h-[22px] w-[1px] bg-primary-500" />
+                        <p className="text-[14px] font-medium leading-[22px] uppercase text-primary-500">we love developers</p>
+                    </div>
+                    <h2 className="max-w-[580px] text-[32px] md:text-[48px] leading-10 md:leading-[60px] font-normal text-center text-grayscale-900">They talk about it <br /> better than us</h2>
                 </div>
-                <h2 className="max-w-[580px] text-[48px] leading-[60px] font-normal text-center text-grayscale-900">They talk about it <br /> better than us</h2>
-                </div>
-                <div className="flex flex-row">
-                    <div className="flex flex-col h-[448px] justify-between px-16 py-14 bg-additional-salty rounded-tl-[16px] rounded-bl-[16px]">
-                        <p className="text-[28px] leading-[48px] font-normal text-grayscale-900">“SoftQA helps us deploy faster and increase our efficiency by 80%. Because of the confidence that we have on SoftQA we are able to test fast and release faster. It helps us debug our code easily, reduce developer feedback time, and helps in quickly implementing our go-to-market strategy.”</p>
+                <div className="flex flex-col-reverse md:flex-row items-stretch">
+                    <div className="flex flex-row md:hidden gap-2 mt-6 items-center justify-center">
+                        <div className="bg-transparent cursor-pointer hover:bg-primary-50 rounded-[12px] w-12 h-12 flex flex-col justify-center items-center">
+                            <Image src="/home/testimonials/arrow-left.png" alt="testimonial" width={20} height={20}/>
+                        </div>
+                        <div className="bg-primary-50 cursor-pointer hover:bg-primary-100 rounded-[12px] w-12 h-12 flex flex-col justify-center items-center">
+                            <Image src="/home/testimonials/arrow-right.png" alt="testimonial" width={20} height={20}/>
+                        </div>
+                    </div>
+                    <div className="flex flex-col flex-1 min-h-[448px] justify-between md:px-10 lg:px-16 p-6 md:py-12 lg:py-14 bg-additional-salty rounded-b-[8px] md:rounded-b-[0px] md:rounded-tl-[16px] md:rounded-bl-[16px]">
+                        <p className="text-[20px] md:text-[28px] leading-9 md:leading-[48px] font-normal text-grayscale-900">“SoftQA helps us deploy faster and increase our efficiency by 80%. Because of the confidence that we have on SoftQA we are able to test fast and release faster. It helps us debug our code easily, reduce developer feedback time, and helps in quickly implementing our go-to-market strategy.”</p>
                         <div className="flex flex-row justify-between items-center">
                         <div className="flex flex-row gap-5 items-center">
-                                <Image src="/home/testimonials/malik.png" alt="testimonial" width={64} height={64} />
+                                <Image src="/home/testimonials/malik.png" alt="testimonial" width={64} height={64} className="w-12 h-12 md:w-16 md:h-16"/>
                                 <div className="flex flex-col gap-1.5">
-                                    <p className="text-[18px] font-normal text-grayscale-900 leading-[28px]">Malik Ganteng</p>
-                                    <p className="text-[16px] font-normal leading-[24px] text-grayscale-600">Senior Engineer, Clause</p>
+                                    <p className="text-[16px] md:text-[18px] font-normal text-grayscale-900 leading-6 md:leading-[28px]">Malik Ganteng</p>
+                                    <p className="text-[14px] leading-[22px] md:text-[16px] font-normal md:leading-[24px] text-grayscale-600">Senior Engineer, Clause</p>
                                 </div>
                             </div>
-                            <div className="flex flex-col">
+                            <div className="hidden md:flex md:flex-col">
                                 <Image src="/home/testimonials/arrow-up.png" alt="testimonial" width={32} height={32} />
                                 <Image src="/home/testimonials/arrow-down.png" alt="testimonial" width={32} height={32} />
                             </div> 
                         </div>
                     </div>
-                    <div className="flex flex-col gap-2">
-                        <div className="bg-additional-salty w-7 h-[106px] rounded-tr-[16px] rounded-br-[8px]"></div>
-                        <div className="bg-additional-palm w-7 h-[106px] rounded-tr-[8px] rounded-br-[8px]"></div>
-                        <div className="bg-additional-light w-7 h-[106px] rounded-tr-[8px] rounded-br-[8px]"></div>
-                        <div className="bg-additional-snow w-7 h-[106px] rounded-tr-[8px] rounded-br-[8px]"></div>
+                    <div className="hidden md:flex flex-col w-7 gap-2">
+                        <div className="flex-1 bg-additional-salty rounded-tr-[16px] rounded-br-[8px]"></div>
+                        <div className="flex-1 bg-additional-palm rounded-tr-[8px] rounded-br-[8px]" ></div>
+                        <div className="flex-1 bg-additional-light rounded-tr-[8px] rounded-br-[8px]"></div>
+                        <div className="flex-1 bg-additional-snow rounded-tr-[8px] rounded-br-[8px]"></div>
+                    </div>
+                    <div className="flex md:hidden flex-row h-4 gap-1">
+                        <div className="w-1/4 bg-additional-salty rounded-t-[8px]"></div>
+                        <div className="w-1/4 bg-additional-palm rounded-t-[8px]"></div>
+                        <div className="w-1/4 bg-additional-light rounded-t-[8px]"></div>
+                        <div className="w-1/4 bg-additional-snow rounded-t-[8px]"></div>
                     </div>
                 </div>
             </div>
