184f9f3983016d0c31bb51a7b5d607ef5beb8707
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-07-22 20:58:39 +0700

    style(modal): update responsive sizing for talk to sales modal on tablet and mobile

--- src/components/home/modal.tsx
+++ src/components/home/modal.tsx
@@ -17,18 +17,18 @@ export function HomeModal() {
                 Talk to Sales       
             </Button>
         </DialogTrigger> 
-        <DialogContent className="w-full max-w-[584px] overflow-y-auto max-h-[90vh]">
+        <DialogContent className="w-full md:max-w-[584px] max-w-[90vw] overflow-y-auto max-h-[90vh]">
             <div className="flex flex-col w-full gap-6">
                 <div className="flex flex-col gap-2">
                     <DialogHeader>
                         <DialogTitle>Contact our sales team! </DialogTitle>
                     </DialogHeader>
                     <div className="relative">
-                        <p className="text-[18px] leading-7 text-grayscale-900">Just few details and your questions answered by one of our product experts.</p>
-                        <Image src="/home/sales.png" alt="icon" width={72} height={28} className="absolute -bottom-1 left-35 object-contain"/>
+                        <p className="text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-900">Just few details and your questions answered by one of our product <br className="block md:hidden"/> experts.</p>
+                        <Image src="/home/sales.png" alt="icon" width={72} height={28} className="absolute bottom-0 md:-bottom-1 left-15 md:left-35 object-contain md:w-[72px] md:h-[28px] w-[60px] h-6"/>
                     </div>
                 </div>
-                <div className="flex flex-row gap-6">
+                <div className="flex flex-col md:flex-row gap-6">
                     <div className="flex flex-col w-full gap-2">
                         <span className="text-[16px] leading-6 text-grayscale-900">First name*</span>
                         <InputEmail placeholder="Enter first name"/>
@@ -42,7 +42,7 @@ export function HomeModal() {
                     <span className="text-[16px] leading-6 text-grayscale-900">Your Email*</span>
                     <InputEmail placeholder="Enter your email"/>
                 </div>
-                <div className="flex flex-row gap-6">
+                <div className="flex flex-col md:flex-row gap-6">
                     <div className="flex flex-col w-full gap-2">
                         <span className="text-[16px] leading-6 text-grayscale-900">Region*</span>
                         <div className="inline-flex w-full h-14 justify-between px-4 items-center rounded-[12px] border-2 border-grayscale-100">
