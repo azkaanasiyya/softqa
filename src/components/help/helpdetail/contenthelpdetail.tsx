480a33f84f2d0db52a1d115baf1831fec332f7b5
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-07-22 14:37:08 +0700

    style(helpdetail): adjust responsive layout for tablet and mobile

--- src/components/help/helpdetail/contenthelpdetail.tsx
+++ src/components/help/helpdetail/contenthelpdetail.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ContentHelpDetail() {
     return (
-        <div className="flex flex-col justify-center items-center bg-primary-900 px-[124px] pt-8 pb-16">
+        <div className="flex flex-col justify-center items-center bg-primary-900 px-6 md:px-10 lg:px-[124px] pt-6 lg:pt-8 pb-16">
             <div className="max-w-[1192px] w-full flex flex-col gap-12">
                 <div className="flex flex-row gap-1 items-center">
                     <Link href="/help" className="cursor-pointer">
@@ -11,7 +11,7 @@ export default function ContentHelpDetail() {
                     <span className="text-[16px] leading-6 text-base-white w-4 flex flex-col justify-center items-center">/</span>
                     <span className="text-[16px] leading-6 text-white-opacity-80">General questions</span>
                 </div>
-                <h1 className="text-[56px] leading-[70px] text-base-white text-center">General questions</h1>
+                <h1 className="text-[40px] md:text-[56px] leading-[50px] md:leading-[70px] text-base-white text-center">General questions</h1>
             </div>
         </div>
     )
--- src/components/help/helpdetail/maincontent.tsx
+++ src/components/help/helpdetail/maincontent.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function MainContent() {
     return (
-        <div className="flex flex-col justify-center items-center bg-base-white px-[124px] pt-[104px] pb-[116px]">
-            <div className="max-w-[1192px] flex flex-row gap-8 items-start">
-                <div className="flex flex-col gap-1 rounded-[16px] bg-grayscale-50 p-3 w-[376px]">
+        <div className="flex flex-col justify-center items-center bg-base-white px-6 md:px-8 lg:px-[124px] py-12 md:py-16 lg:pt-[104px] lg:pb-[116px]">
+            <div className="max-w-[1192px] flex flex-col lg:flex-row gap-12 lg:gap-8 items-start">
+                <div className="md:flex md:flex-col gap-1 rounded-[16px] bg-grayscale-50 p-3 w-full lg:max-w-[376px]">
                     <div className="flex flex-col py-3 px-4 rounded-[12px] bg-base-white text-[18px] leading-7 text-primary-500">How do I create an account?</div>
                     <div className="flex flex-col py-3 px-4 text-[18px] leading-7 text-grayscale-900">How do I update my profile information?</div>
                     <div className="flex flex-col py-3 px-4 text-[18px] leading-7 text-grayscale-900">How can I delete my account?</div>
@@ -12,23 +12,27 @@ export default function MainContent() {
                     <div className="flex flex-col py-3 px-4 text-[18px] leading-7 text-grayscale-900">Understand main dev account</div>
                     <div className="flex flex-col py-3 px-4 text-[18px] leading-7 text-grayscale-900">Collaborate with team in a workspace</div>
                 </div>
-                <div className="flex flex-col gap-12 pl-12 pr-[102px] w-[784px]">
+                <div className="flex flex-row justify-between items-center w-full md:hidden py-[15px] px-4 border-2 border-grayscale-100 rounded-[12px]">
+                    <span className="text-[16px] leading-6 font-medium text-grayscale-900">How do I create an account?</span>
+                    <Image src="/help/list.png" alt="icon" width={20} height={20} />
+                </div>
+                <div className="flex flex-col gap-12 lg:pl-12 lg:pr-[102px] max-w-[784px] w-full">
                     <div className="flex flex-col gap-6">
-                        <h3 className="text-[40px] leading-[50px] text-grayscale-900">How do I create an account?</h3>
+                        <h3 className="text-[32px] md:text-[40px] leading-10 md:leading-[50px] text-grayscale-900">How do I create an account?</h3>
                         <div className="flex flex-col gap-3">
                             <div className="flex flex-row gap-3 items-center">
                                 <div className="flex flex-col justify-center items-center px-1 w-5 h-5 bg-primary-500 rounded-full text-[14px] leading-[18px] text-base-white font-medium">1</div>
-                                <span className="text-[18px] leading-7 text-grayscale-500">From your device, go to the Google Account sign in page.</span>
+                                <span className="text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500">From your device, go to the Google Account sign in page.</span>
                             </div>
                             <div className="flex flex-row gap-3 items-center">
                                 <div className="flex flex-col justify-center items-center px-1 w-5 h-5 bg-primary-500 rounded-full text-[14px] leading-[18px] text-base-white font-medium">2</div>
-                                <span className="text-[18px] leading-7 text-grayscale-500">Click Create account.</span>
+                                <span className="text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500">Click Create account.</span>
                             </div>
                             <div className="flex flex-row gap-3 items-start">
                                 <div className="flex flex-col justify-center items-center px-1 w-5 h-5 mt-1 bg-primary-500 rounded-full text-[14px] leading-[18px] text-base-white font-medium">3</div>
                                 <div className="flex flex-col">
-                                   <span className="text-[18px] leading-7 text-grayscale-500">In the drop down, select if the account is for your:</span>
-                                    <ul className="list-disc text-[18px] leading-7 text-grayscale-500 pl-7">
+                                   <span className="text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500">In the drop down, select if the account is for your:</span>
+                                    <ul className="list-disc text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500 pl-7">
                                         <li>Personal use</li>
                                         <li>Child</li>
                                         <li>Work or business</li>
@@ -37,11 +41,11 @@ export default function MainContent() {
                             </div>
                             <div className="flex flex-row gap-3 items-center">
                                 <div className="flex flex-col justify-center items-center px-1 w-5 h-5 bg-primary-500 rounded-full text-[14px] leading-[18px] text-base-white font-medium">4</div>
-                                <span className="text-[18px] leading-7 text-grayscale-500">To set up your account, follow the steps on the screen.</span>
+                                <span className="text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500">To set up your account, follow the steps on the screen.</span>
                             </div>
                         </div>
                         <div className="bg-primary-50 py-3 px-5 rounded-[16px] border border-primary-100">
-                            <p className="text-[18px] leading-7 text-grayscale-500">
+                            <p className="text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500">
                                 <span className="font-medium text-grayscale-900">Tip:</span> To use Gmail for your business, a Google Workspace account might be better for you than a personal Google Account. With Google Workspace, you get increased storage, professional email addresses, and additional features.
                             </p>
                         </div>
@@ -49,34 +53,34 @@ export default function MainContent() {
                     <div className="flex flex-col gap-6 pt-10 border-t border-grayscale-100">
                         <h5 className="text-[24px] leading-[30px] text-grayscale-900">The username I want is taken</h5>
                         <div className="flex flex-col gap-3">
-                            <span className="text-[18px] leading-7 text-grayscale-500">You can’t create a Gmail address if the username you requested is:</span>
+                            <span className="text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500">You can’t create a Gmail address if the username you requested is:</span>
                             <div>
-                                <ul className="list-disc text-[18px] leading-7 text-grayscale-500 pl-6">
+                                <ul className="list-disc text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500 pl-6">
                                     <li>Already being used</li>
                                 </ul>  
                             </div>
                             <div>
-                                <ul className="list-disc text-[18px] leading-7 text-grayscale-500 pl-6">
+                                <ul className="list-disc text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500 pl-6">
                                     <li>Very similar to an existing username.</li>
-                                    <ul className="list-disc text-[18px] leading-7 text-grayscale-500 pl-7">
+                                    <ul className="list-disc text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500 pl-7">
                                         <li>For example, if example@gmail.com already exists, you can&apos;t use examp1e@gmail.com.</li>
                                     </ul>
                                 </ul>  
                             </div>
                             <div>
-                                <ul className="list-disc text-[18px] leading-7 text-grayscale-500 pl-6">
+                                <ul className="list-disc text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500 pl-6">
                                     <li>The same as a username that someone used in the past and then deleted.</li>
                                 </ul>  
                             </div>
                             <div>
-                                <ul className="list-disc text-[18px] leading-7 text-grayscale-500 pl-6">
+                                <ul className="list-disc text-[16px] md:text-[18px] leading-6 md:leading-7 text-grayscale-500 pl-6">
                                     <li>Reserved by Google to prevent spam or abuse.</li>
                                 </ul>  
                             </div>
                         </div>
                     </div>
                     <div className="bg-primary-50 flex flex-col items-center gap-4 rounded-[12px] p-6">
-                        <h5 className="text-[24px] leading-[30px] text-grayscale-900">Was this article helpful?</h5>
+                        <h5 className="text-[20px] md:text-[24px] leading-9 md:leading-[30px] text-grayscale-900">Was this article helpful?</h5>
                         <div className="flex flex-col items-center gap-2">
                             <div className="flex flex-row gap-4">
                                 <div className="flex flex-row gap-2 items-center bg-base-white rounded-[8px] border-2 border-grayscale-100 px-6 py-3">
