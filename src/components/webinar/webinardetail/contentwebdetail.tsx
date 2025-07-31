def1c37ef4bd33c3bab2515fd51c0258fb381dcb
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-07-31 14:59:34 +0700

    test

--- src/components/webinar/webinardetail/contentwebdetail.tsx
+++ src/components/webinar/webinardetail/contentwebdetail.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { MobileSectionSelect } from "./selectid";
 import FadeInSection from "@/components/animation/fadein";
 
