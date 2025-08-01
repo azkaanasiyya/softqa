212e29ac96c15d20e96585ded427a6433e84afcf
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-08-01 14:42:27 +0700

    feat(home): adjust color text on modal talk to sales

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,10 +9,10 @@ const variantClasses: Record<Required<TextareaProps>["variant"], string> = {
   default: "",
 
   sales:
-    "placeholder:text-grayscale-400 focus:border-[1.5px] focus:border-primary-500 focus-visible:ring-[3px] focus-visible:ring-[#24697514] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 bg-base-white aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-[148px] w-full max-w-[520px] border-2 border-grayscale-100 p-4 text-[16px] leading-6 text-grayscale-400 rounded-[12px] transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+    "placeholder:text-grayscale-400 focus:border-[1.5px] focus:border-primary-500 focus-visible:ring-[3px] focus-visible:ring-[#24697514] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 bg-base-white aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-[148px] w-full max-w-[520px] border-2 border-grayscale-100 p-4 text-[16px] leading-6 text-grayscale-900 rounded-[12px] transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
 
   help:
-    "bg-transparent border border-transparent text-muted-foreground placeholder:text-muted-foreground focus-visible:ring-0 focus-visible:border-muted-foreground",
+    "bg-transparent border border-transparent text-grayscale-900 placeholder:text-grayscale-400 focus-visible:ring-0 focus-visible:border-muted-foreground",
 }
 
 function Textarea({
@@ -24,7 +24,7 @@ function Textarea({
     <textarea
       data-slot="textarea"
       className={cn(
-        "placeholder:text-muted-foreground focus:border-[1.5px] focus:border-primary-500 focus-visible:ring-[3px] focus-visible:ring-[#24697514] focus:outline-none bg-base-white aria-invalid:border-destructive flex field-sizing-content min-h-[148px] w-full max-w-[520px] border-2 border-grayscale-100 p-4 text-[16px] leading-6 text-grayscale-400 rounded-[12px] transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "placeholder:text-grayscale-400 focus:border-[1.5px] focus:border-primary-500 focus-visible:ring-[3px] focus-visible:ring-[#24697514] focus:outline-none bg-base-white aria-invalid:border-destructive flex field-sizing-content min-h-[148px] w-full max-w-[520px] border-2 border-grayscale-100 p-4 text-[16px] leading-6 text-grayscale-900 rounded-[12px] transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
         variantClasses[variant],
         className
       )}
