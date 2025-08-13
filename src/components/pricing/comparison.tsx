bf226a567f669008560f6fb082d3cec0d5f6c414
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-08-13 10:24:00 +0700

    style: adjust size cols comparison

--- src/components/pricing/comparison.tsx
+++ src/components/pricing/comparison.tsx
@@ -26,7 +26,7 @@ export default function PricingComparison() {
                     <div className="bg-base-white border-b border-l border-grayscale-100 h-[72px] py-6 px-7 text-[18px] leading-7 font-normal text-grayscale-900">AI Features</div>
                     <div className="bg-base-white border-b border-l border-grayscale-100 h-[72px] rounded-bl-[12px] py-6 px-7 text-[18px] leading-7 font-normal text-grayscale-900">Training & Onboarding</div>
                 </div>
-                <div className="bg-base-white hidden lg:flex lg:flex-col max-w-[270.33px]">
+                <div className="bg-base-white hidden lg:flex lg:flex-col lg:max-w-[180px] xl:max-w-[270.33px]">
                     <div className="flex flex-col items-center gap-0.5 h-[88px] rounded-tl-[16px] border-t border-l border-b border-grayscale-100 py-4">
                         <p className="text-[16px] leading-6 text-grayscale-500 font-normal">Starter</p>
                         <h5 className="text-[24px] leading-[30px] text-grayscale-900 font-normal">$29</h5>
@@ -66,11 +66,11 @@ export default function PricingComparison() {
                     </div>
                     <div className="border-b border-l border-grayscale-100 rounded-bl-[16px] h-[96px] py-6 px-5 flex flex-col justify-center items-center">
                         <Link href="/auth/sign-up" className="w-full block">
-                            <Button variant="login" size="medium" className="cursor-pointer text-primary-500 w-[230.33px]">Get Started</Button>
+                            <Button variant="login" size="medium" className="cursor-pointer text-primary-500 w-full xl:w-[230.33px]">Get Started</Button>
                         </Link>
                     </div>
                 </div>
-                <div className="bg-base-white hidden lg:flex lg:flex-col max-w-[270.33px]">
+                <div className="bg-base-white hidden lg:flex lg:flex-col lg:max-w-[180px] xl:max-w-[270.33px]">
                     <div className="bg-primary-900 flex flex-col items-center gap-0.5 h-[88px] border-t border-l border-b border-grayscale-100 py-4">
                         <p className="text-[16px] leading-6 text-[#FFFFFFEB] font-normal">Team</p>
                         <h5 className="text-[24px] leading-[30px] text-base-white font-normal">$99</h5>
@@ -110,11 +110,11 @@ export default function PricingComparison() {
                     </div>
                     <div className="bg-grayscale-50 border-b border-l border-grayscale-100 h-[96px] py-6 px-5 flex flex-col justify-center items-center">
                         <Link href="/auth/sign-up" className="w-full block">
-                            <Button variant="request" size="medium" className="cursor-pointer text-cyan-500 w-[230.33px]">Get Started</Button>
+                            <Button variant="request" size="medium" className="cursor-pointer text-cyan-500 w-full xl:w-[230.33px]">Get Started</Button>
                         </Link>
                     </div>
                 </div>
-                <div className="bg-base-white hidden lg:flex lg:flex-col max-w-[270.33px]">
+                <div className="bg-base-white hidden lg:flex lg:flex-col lg:max-w-[180px] xl:max-w-[270.33px]">
                     <div className="flex flex-col items-center gap-0.5 h-[88px] rounded-tr-[16px] border border-grayscale-100 py-4">
                         <p className="text-[16px] leading-6 text-grayscale-500 font-normal">Enterprise</p>
                         <h5 className="text-[24px] leading-[30px] text-grayscale-900 font-normal">Custom</h5>
@@ -154,7 +154,7 @@ export default function PricingComparison() {
                     </div>
                     <div className="border-b border-l border-r border-grayscale-100 rounded-br-[16px] h-[96px] py-6 px-5 flex flex-col justify-center items-center">
                         <Link href="/auth/sign-up" className="w-full block">
-                            <Button variant="login" size="medium" className="cursor-pointer text-primary-500 w-[230.33px]">Get Started</Button>
+                            <Button variant="login" size="medium" className="cursor-pointer text-primary-500 w-full xl:w-[230.33px]">Get Started</Button>
                         </Link>
                     </div>
                 </div>
